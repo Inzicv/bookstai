f06bc89d065f40795acb9fc42af508670a8a0a31
Inzicv <142996930+Inzicv@users.noreply.github.com>
2026-06-30 14:24:05 +0200

    feat: add BookstAI Review and Song components with workflows

---
+++ langflow_components/bookstai/__init__.py
@@ -0,0 +1,4 @@
+﻿from .bookstai_review_component import BookstAIReviewComponent
+from .bookstai_song_component import BookstAISongComponent
+
+__all__ = ["BookstAIReviewComponent", "BookstAISongComponent"]
